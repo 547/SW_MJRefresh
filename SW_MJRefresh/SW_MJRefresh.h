2c151f9a27ad605440dffbaa2d1953079f8c7fc0
Seven Wang <seven.wang@supernovachina.com>
2019-06-19 16:44:42 +0800

    Initial Commit

---
+++ SW_MJRefresh/SW_MJRefresh.h
@@ -0,0 +1,19 @@
+//
+//  SW_MJRefresh.h
+//  SW_MJRefresh
+//
+//  Created by Supernova SanDick SSD on 2019/6/19.
+//  Copyright © 2019 Seven. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for SW_MJRefresh.
+FOUNDATION_EXPORT double SW_MJRefreshVersionNumber;
+
+//! Project version string for SW_MJRefresh.
+FOUNDATION_EXPORT const unsigned char SW_MJRefreshVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SW_MJRefresh/PublicHeader.h>
+
+
