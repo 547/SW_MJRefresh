9ccdad9a6266a34be287a9444ebc2fdaa6930620
timer <timer_sevenwang@163.com>
2019-12-02 15:44:52 +0800

    update

--- SW_MJRefresh/ScrollView.swift
+++ SW_MJRefresh/ScrollView.swift
@@ -11,37 +11,37 @@ import UIKit
 import MJRefresh
 extension UIScrollView {
     public func setRefreshView(refreshBlock:(() -> ())?,loadMoreBlock:(() -> ())?) -> (){
-        if refreshBlock != nil {
+        if let refreshBlock = refreshBlock {
             let headView = MJRefreshNormalHeader.init(refreshingBlock: refreshBlock)
             headView?.lastUpdatedTimeLabel.isHidden = true
             mj_header = headView
         }
-        if loadMoreBlock != nil {
+        if let loadMoreBlock = loadMoreBlock {
             let footView = MJRefreshAutoNormalFooter.init(refreshingBlock: loadMoreBlock)
             mj_footer = footView
             mj_footer.isHidden = true
         }
     }
     public func headerView(hidden:Bool) -> () {
-        if mj_header != nil, mj_header.isHidden != hidden {
+        if let mj_header = mj_header, mj_header.isHidden != hidden {
             mj_header.isHidden = hidden
         }
     }
     public func footerView(hidden:Bool) -> () {
-        if mj_footer != nil, mj_footer.isHidden != hidden {
+        if let mj_footer = mj_footer, mj_footer.isHidden != hidden {
             mj_footer.isHidden = hidden
         }
     }
     public func stopRefresh() -> () {
-        if mj_footer != nil && mj_footer.isRefreshing {
+        if let mj_footer = mj_footer, mj_footer.isRefreshing {
             mj_footer.endRefreshing()
         }
-        if mj_header != nil && mj_header.isRefreshing {
+        if let mj_header = mj_header, mj_header.isRefreshing {
             mj_header.endRefreshing()
         }
     }
     public func refresh() -> () {
-        if mj_header != nil{
+        if let mj_header = mj_header {
             mj_header.beginRefreshing()
         }
     }
